d28c58bcda3e46cd016c6a05af94656c4bdecb40
Exhodus <exhodus87@gmail.com>
2024-04-20 20:37:03 -0400

    Small changes - removed the Twitter_Auth const, since it could be visible from console - did some work around git interactions - now the project will auto pull the latest code from prod and the latest Digest with state before running

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ const { TwitterApi } = require('twitter-api-v2');
 const { getContractEvents } = require('./blockChain.js');
 const { groupData, toMarkdown, writeToFile } = require('./digest.js');
 const { formatTweet } = require('./utils.js');
-const { pushToGit, waitForWorkflowCompletion } = require('./git.js');
+const { gitPushDigest, gitPull, waitForWorkflowCompletion } = require('./git.js');
 
 require('dotenv').config();
 
-const TWITTER_AUTH = {
+const client = new TwitterApi({
     appKey: process.env.twitter_appKey,
     appSecret: process.env.twitter_appSecret,
     accessToken: process.env.twitter_accessToken,
     accessSecret: process.env.twitter_accessSecret,
-}
+});
 
-const client = new TwitterApi(TWITTER_AUTH);
+async function main() {
+    gitPull('./', process.env.MAIN_REPO, 'prod');
+    gitPull('./Digest', process.env.DIGEST_REPO);
 
-async function main() {     
     const data = await getContractEvents().then(groupData);
     const digest = toMarkdown(data);
     writeToFile("./Digest/docs/" + digest.filename, digest.markdown);
-    pushToGit("Adding new digest at "+new Date());
+    gitPushDigest("Adding new digest at "+new Date());
     const result = await waitForWorkflowCompletion(); // if the result is 'failure' use the link to the .md instead of the page
     await client.v2.tweet(formatTweet(data, digest.filename, result === "success"));
 
